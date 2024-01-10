1988a6c2dc1cc1dd9be62e6fc85936faf5976d65
Adarsh Pandey <143625068+adarshpandey18@users.noreply.github.com>
2024-01-10 20:20:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (26.99%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int left = binarySearch(nums, target, true);
+        int right = binarySearch(nums, target, false);
+        return new int[] {left, right};
+    }
+    public int binarySearch(int[]nums, int target, boolean flag) {
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                ans = mid;
+                if(flag)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
