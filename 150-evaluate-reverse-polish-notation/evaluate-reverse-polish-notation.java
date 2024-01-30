1d53dce44bf3ac87190d800f0862172dc5378ecb
Adarsh Pandey <143625068+adarshpandey18@users.noreply.github.com>
2024-01-30 20:20:15 +0530

    Time: 6 ms (78.23%) | Memory: 45 MB (15.49%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -3,24 +3,36 @@ import java.util.Stack;
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
+
         for (String token : tokens) {
-            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+            if (isOperator(token)) {
                 int num2 = stack.pop();
                 int num1 = stack.pop();
-                int num3;
-                if (token.equals("+"))
-                    num3 = num1 + num2;
-                else if (token.equals("-"))
-                    num3 = num1 - num2;
-                else if (token.equals("*"))
-                    num3 = num1 * num2;
-                else
-                    num3 = num1 / num2;
-                stack.push(num3);
+                stack.push(performOperation(num1, num2, token));
             } else {
                 stack.push(Integer.parseInt(token));
             }
         }
+
         return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int performOperation(int num1, int num2, String operator) {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                return num1 / num2;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
     }
 }
