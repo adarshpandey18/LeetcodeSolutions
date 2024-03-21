513ef5042d1c92408c4154782d4eb791c7914349
Adarsh Pandey <143625068+adarshpandey18@users.noreply.github.com>
2024-03-21 10:56:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (40.27%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -1,23 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        List<Integer> list = new ArrayList<>();
-        
-        while (head != null) {
-            list.add(head.val);
-            head = head.next;
+        //finding Index 
+        ListNode temporary = head;
+        int size =0;
+        while(temporary != null) {
+            size++;
+            temporary = temporary.next;
         }
-        if(list.size()== 0)
-            return head;
-        ListNode ans = new ListNode();
-        ListNode node = ans;
-
-        node.val = list.get(list.size() - 1);
-        for (int i = list.size() - 2; i >= 0; i--) {
-            ListNode temp = new ListNode(list.get(i));
-            node.next = temp;
-            node = node.next;
+        if (size == 0) {
+            ListNode ans = null;
+            return ans;
         }
-
-        return ans;
+        int[]nodes = new int[size];
+        ListNode temp = head;
+        int index = 0;
+        while(temp != null) {
+            nodes[index++] = temp.val;
+            temp = temp.next;
+        }
+        ListNode ans = new ListNode(nodes[size-1]);
+        ListNode pointer = ans;
+        for(int i = nodes.length - 2; i >=0; i--) {
+            ListNode t = new ListNode(nodes[i]);
+            ans.next = t;
+            ans = ans.next;
+        }
+        return pointer;
     }
 }
